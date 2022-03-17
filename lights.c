10b61f710558910b783fe6dc3ed6ba3aaf25a2ed
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-03-17 22:21:13 +0000

    remove unused functions

--- lights.c
+++ lights.c
@@ -15,65 +15,16 @@ PointLight Get_Point_Light(float x, float y, float z,
     return p;
 }
 
-__m128 Calculate_Point_Light_Colour(const PointLight pl, __m128 normal, __m128 frag_position)
-{
-    //// calculate vertex to light data
-    // const __m128 vert_to_light = _mm_sub_ps(pl.position, vert);
-
-    //// calculate distance
-    // vert.m128_f32[3] = 0.0f;
-
-    // const float distance_to_light = (const float)sqrt((double)hsum_ps_sse3(_mm_mul_ps(vert_to_light, vert_to_light)));
-
-    //// calculate direction
-    // const __m128 direction_to_light = Normalize_m128(_mm_sub_ps(vert_to_light, _mm_set1_ps(distance_to_light)));
-
-    // const float attenulation_value = 1.0f / (pl.constant_attenuation +
-    //                                          pl.linear_attenuation * distance_to_light +
-    //                                          pl.quadradic_attenuation * distance_to_light * distance_to_light);
-
-    const float specular_strength = 0.8f;
-    const float ambient_value = 0.1f;
-    normal = Normalize_m128(normal);
-
-    // AMBIENT
-    const __m128 ambient = _mm_set1_ps(ambient_value);
-
-    const __m128 light_position = _mm_set_ps(0.0f, 2.0f, 0.0f, -1.0f);
-    const __m128 light_direction = Normalize_m128(_mm_sub_ps(light_position, frag_position));
-    const __m128 negative_light_direction = _mm_mul_ps(light_direction, _mm_set1_ps(-1.0f));
-
-    // DIFFUSE
-    const float diffuse_amount = (const float)fmax((double)Calculate_Dot_Product_SIMD(normal, light_direction), 0.0);
-    // const float diffuse = attenulation_value * (const float)fmax((double)Calculate_Dot_Product_SIMD(normal, direction_to_light), 0.0);
-    const __m128 diffuse_colour = _mm_set_ps(1.0f, 0.0f, 0.0f, 1.0f);
-    const __m128 diffuse = _mm_mul_ps(_mm_set1_ps(diffuse_amount), diffuse_colour);
-
-    // SPECULAR
-    const __m128 camera_position = _mm_set_ps(0.0f, 0.0f, 0.0f, 0.0f);
-    const __m128 view_direction = Normalize_m128(_mm_sub_ps(camera_position, frag_position));
-    const __m128 half_way_direction = Normalize_m128(_mm_add_ps(light_direction, view_direction));
-
-    const __m128 reflection_direction = Normalize_m128(Reflect_m128(negative_light_direction, normal));
-
-    // const float spec_amount = (const float)pow(fmax(Calculate_Dot_Product_SIMD(view_direction, reflection_direction), 0.0), 32);
-    const float spec_amount = (const float)pow(fmax(Calculate_Dot_Product_SIMD(normal, half_way_direction), 0.0), 16);
-
-    const __m128 specular_colour = _mm_set_ps(1.0f, 1.0f, 0.0f, 0.0f);
-    const __m128 specular = _mm_mul_ps(specular_colour, _mm_set1_ps(spec_amount * specular_strength));
-
-    const __m128 final_colour = _mm_add_ps(_mm_add_ps(ambient, diffuse), specular);
-
-    return final_colour;
-}
-
-// https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/reflect.xhtml
-// I - 2.0 * dot(N, I) * N
 __m128 Reflect_m128(const __m128 I, __m128 N)
 {
+    // https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/reflect.xhtml
+    // I - 2.0 * dot(N, I) * N
+
     N = Normalize_m128(N);
+
     __m128 righ = _mm_mul_ps(_mm_set1_ps(Calculate_Dot_Product_SIMD(N, I)), N);
     righ = _mm_mul_ps(righ, _mm_set1_ps(2.0f));
+
     return _mm_sub_ps(I, righ);
 }
 
--- lights.h
+++ lights.h
@@ -28,17 +28,8 @@ typedef struct PointLight_s
 } PointLight;
 
 PointLight Get_Point_Light(float x, float y, float z, float constant_atten, float linear_atten, float quad_atten);
-__m128 Calculate_Point_Light_Colour(const PointLight pl, __m128 normal, __m128 vert);
-__m128 Specular_Highlight_Colour(const __m128 view_direction, const __m128 light_direction, const __m128 normal);
 
 __m128 Reflect_m128(const __m128 I, const __m128 N);
-
-__m128 Calculate_Normal_Mapping_Colour(const unsigned char *diffuse_texture,
-                                       const unsigned char *normal_texture,
-                                       const Mat4x4 TBN,
-                                       const __m128 Tangent_Frag_Pos,
-                                       const __m128 Tangent_Light_Pos,
-                                       const __m128 Tangent_View_Pos);
 
 __m128 Get_Diffuse_Amount(const __m128 light_direction, const __m128 contact_position, const __m128 normal);
 __m128 Get_Specular_Amount(const __m128 view_direction, const __m128 light_direction, const __m128 normal, const double strength, const double power_value);
